0c71650b89f1484f0bb6c3959c2b5ae69228be99
Jonas-Ian Kuche <jonas-ian.kuche@mni.thm.de>
2021-03-17 08:50:00 +0100

    Improve backend integration
    Signed-off-by: Jonas-Ian Kuche <jonas-ian.kuche@mni.thm.de>

--- web-gui/src/app/service/task-points.service.ts
+++ web-gui/src/app/service/task-points.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable, of, from, throwError} from 'rxjs';
+import {catchError, mergeMap, map, filter, toArray} from 'rxjs/operators';
 import {Requirement} from '../model/Requirement';
 import {Task} from '../model/Task';
 
@@ -8,52 +9,6 @@ import {Task} from '../model/Task';
   providedIn: 'root'
 })
 export class TaskPointsService {
-
-  requirements: Requirement[] = [
-    {
-      id: 1,
-      toPass: 1,
-      tasks: [{
-        id: 5,
-        name: 'Aufgabe 1',
-        description: 'string',
-        deadline: 'st'
-      }],
-      bonusFormula: 'true',
-      hidePoints: false
-    },
-    {
-      id: 2,
-      toPass: 2,
-      tasks: [{
-        id: 2,
-        name: 'Aufgabe 2a',
-        description: 'string',
-        deadline: 'st'
-      },
-        {
-          id: 1,
-          name: 'Aufgabe 1a',
-          description: 'string',
-          deadline: 'st'
-        }],
-      bonusFormula: 'Wrong Formula',
-      hidePoints: false
-    },
-    {
-      id: 3,
-      toPass: 1,
-      tasks: [{
-        id: 5,
-        name: 'Aufgabe 1',
-        description: 'string',
-        deadline: 'st'
-      }],
-      bonusFormula: 'example3',
-      hidePoints: false
-    }
-  ];
-
   constructor(private http: HttpClient) { }
 
   /**
@@ -62,8 +17,7 @@ export class TaskPointsService {
    * @return Observable that succeeds with all requirements of the course
    */
   getAllRequirements(cid: number): Observable<Requirement[]> {
-    // return this.http.get<Requirement[]>(`/api/v1/courses/${cid}/evaluation/container`);
-    return of(this.requirements);
+    return this.http.get<Requirement[]>(`/api/v1/courses/${cid}/evaluation/container`);
   }
 
   /**
@@ -73,8 +27,14 @@ export class TaskPointsService {
    * @return Observable that succeeds with all requirements of the course
    */
   createRequirement(cid: number, requirement: Requirement): Observable<Requirement> { // TODO: input ändern
-    return this.http.post<Requirement>(`/api/v1/courses/${cid}/evaluation/container`, requirement);
-    // return of(this.requirements[1]);
+    return this.http.post<Requirement>(`/api/v1/courses/${cid}/evaluation/container`, requirement).pipe(
+      mergeMap((responseRequirement) => this.setTasks(cid, responseRequirement.id, requirement.tasks).pipe(
+        map((tasksResponse) => {
+          responseRequirement.tasks = tasksResponse;
+          return responseRequirement;
+        }),
+      )),
+    );
   }
 
   /**
@@ -83,9 +43,8 @@ export class TaskPointsService {
    * @param ctid Id of the criteria/requirement
    * @return Observable that succeeds with the requirement
    */
-  getRquirement(cid: number, ctid: number): Observable<Requirement> {
+  getRequirement(cid: number, ctid: number): Observable<Requirement> {
     return this.http.get<Requirement>(`/api/v1/courses/${cid}/evaluation/container/${ctid}`);
-    // return of(this.requirements[1]);
   }
 
   /**
@@ -95,9 +54,15 @@ export class TaskPointsService {
    * @param requirement Updated Requirement
    * @return Observable that succeeds with the updated requirement of the course
    */
-  updateRquirement(cid: number, ctid: number, requirement: Requirement): Observable<Requirement> { // TODO: input ändern
-    return this.http.put<Requirement>(`/api/v1/courses/${cid}/evaluation/container/${ctid}`, requirement);
-    // return of(this.requirements[1]);
+  updateRequirement(cid: number, ctid: number, requirement: Requirement): Observable<Requirement> { // TODO: input ändern
+    return this.http.put<Requirement>(`/api/v1/courses/${cid}/evaluation/container/${ctid}`, requirement).pipe(
+      mergeMap((responseRequirement) => this.setTasks(cid, responseRequirement.id, requirement.tasks).pipe(
+        map((tasksResponse) => {
+          responseRequirement.tasks = tasksResponse;
+          return responseRequirement;
+        }),
+      )),
+    );
   }
 
   /**
@@ -106,9 +71,28 @@ export class TaskPointsService {
    * @param ctid Id of the criteria/requirement
    * @return Observable that succeeds if the requirement does not exists after the operation
    */
-  deleteRquirement(cid: number, ctid: number): Observable<void> {
+  deleteRequirement(cid: number, ctid: number): Observable<void> {
     return this.http.delete<void>(`/api/v1/courses/${cid}/evaluation/container/${ctid}`);
-    // return of();
+  }
+
+  /**
+   * Sets the tasks to the given state
+   * @param cid course id
+   * @param requirementID The id of the requirement
+   * @param tasks The tasks to be added
+   */
+  setTasks(cid: number, requirementID: number, tasks: Task[]): Observable<Task[]> {
+    return this.getRequirement(cid, requirementID).pipe(
+      mergeMap((requirement) => from(requirement.tasks).pipe(
+        filter((task) => tasks.find((reqTask) => task.id === reqTask.id) === undefined),
+        mergeMap((task) => this.removeTask(cid, requirementID, task.id)),
+        toArray(),
+      )),
+      mergeMap(() => from(tasks).pipe(
+        mergeMap((task) => this.addTask(cid, requirementID, task.id)),
+        toArray(),
+      )),
+    );
   }
 
   /**
@@ -118,9 +102,8 @@ export class TaskPointsService {
    * @param taskId The id of the task to be added
    * @return Observable that succeeds with the changed requirement
    */
-  addTask(cid: number, requirementID: string, taskId: string): Observable<Requirement> {
-    // return this.http.post<Requirement>(`/api/v1/courses/${cid}/evaluation/container/${requirementID}/task/${taskId}`);
-    return of(this.requirements[1]);
+  addTask(cid: number, requirementID: number, taskId: number): Observable<Task> {
+    return this.http.post<Task>(`/api/v1/courses/${cid}/evaluation/container/${requirementID}/task/${taskId}`, {});
   }
 
   /**
@@ -130,27 +113,26 @@ export class TaskPointsService {
    * @param taskId The id of the task to be removed
    * @return Observable that succeeds with the changed requirement
    */
-  removeTask(cid: number, requirementID: string, taskId: string): Observable<Requirement> {
-    return this.http.delete<Requirement>(`/api/v1/courses/${cid}/evaluation/container/${requirementID}/task/${taskId}`);
-    // return of(this.requirements[1]);
+  removeTask(cid: number, requirementID: number, taskId: number): Observable<Task> {
+    return this.http.delete<Task>(`/api/v1/courses/${cid}/evaluation/container/${requirementID}/task/${taskId}`);
   }
+
   /**
    * Check the bonus Formula
    * @param bonusFormula The formula for bonus points
-   * @return Observable that succeeds with the status of the formula
+   * @return Observable<string|undefined> that succeeds with the status of the formula
    */
-  checkBonusFormula(bonusFormula: string): Observable<any> { // todo: string?
-    // return this.http.post<any>(`/api/v1/courses/evaluation/formula/validate`, bonusFormula);
-    if (bonusFormula === 'true') {
-      return of({
-        valid: true,
-        message: 'Valider Ausdruck'
-      });
-    } else {
-      return of({
-        valid: false,
-        message: 'Fehler'
-      });
-    }
+  checkBonusFormula(bonusFormula: string): Observable<{valid: boolean, message: string}> {
+    return this.http.post<any>(
+      `/api/v1/courses/evaluation/formula/validate`,
+      {'formula': bonusFormula}
+    ).pipe(
+      catchError((err) => {
+        if (err instanceof HttpErrorResponse && err.status === 400) {
+          return of(err.error);
+        }
+        return throwError(err);
+      }),
+    );
   }
 }
